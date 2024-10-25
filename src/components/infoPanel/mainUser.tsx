89fa1f4cbdc80d32a0f0f596dce6d62d9b951f86
Zer02z2 <zongzechan@gmail.com>
2024-10-25 18:37:44 -0400

    Identifier

--- src/components/infoPanel/mainUser.tsx
+++ src/components/infoPanel/mainUser.tsx
@@ -5,22 +5,31 @@ import { postIpInfo } from "../../fetch/postIpInfo"
 import { IpLog } from "../../config"
 
 export const MainUser = ({ ipLog }: { ipLog: IpLog | undefined }) => {
+  const [identifier, setIdentifier] = useState<string>()
   const [ip, setIp] = useState<string | undefined>()
-  const [location, setLocation] = useState<string | undefined>()
+  const [location, setLocation] = useState<string>()
   const [userName, setUserName] = useState<string>("User")
 
   useEffect(() => {
-    const update = async () => {
+    const init = async () => {
       const myIp = await postIpInfo("ZZ")
-      if (!(myIp && ipLog)) return
-      const myInfo = ipLog[myIp]
+      setIdentifier(myIp)
+    }
+    init()
+  }, [ipLog])
+
+  useEffect(() => {
+    const update = () => {
+      if (!(identifier && ipLog)) return
+      const myInfo = ipLog[identifier]
       const { ip, name, city, country } = myInfo
       setIp(ip)
       setLocation(`${city} - ${country}`)
       setUserName(name)
     }
     update()
-  }, [ipLog])
+  }, [ipLog, identifier])
+
   return (
     <div className="p-8 rounded-lg bg-neutral-800">
       <div className="flex items-center gap-2">
